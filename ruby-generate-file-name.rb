de9ebc1ad0e7443447b3e9a614bfea7e4958b90c
David Vezzani <david_vezzani@yahoo.com>
2015-11-09 17:05:37 -0800

    use markdown (.md) by default instead of text (.txt)

--- ruby-generate-file-name.rb
+++ ruby-generate-file-name.rb
@@ -3,4 +3,4 @@
 title = ARGV[0]
 clean_title = title.gsub(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/, '').downcase.
   gsub(/[^A-Za-z0-9]+/, '-')
-puts "#{Time.now.strftime('%Y%m%d')}-#{clean_title}.txt"
+puts "#{Time.now.strftime('%Y%m%d')}-#{clean_title}.md"
